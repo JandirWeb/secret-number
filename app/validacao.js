d7aee0b7384cbfe53571d36149348792e5920d56
Jandir Moreira <jandirweb@gmail.com>
2024-03-22 16:46:41 -0300

    ajustes para vencedor

--- app/validacao.js
+++ app/validacao.js
@@ -3,16 +3,28 @@ function valueIsValid(chute){
 
     if(chuteInvalido(numero)) {
         elementoChute.innerHTML += '<div class="invalido">Valor inválido!</div>';
+        return
     }
 
     if(numeroInvalido(numero)){
         elementoChute.innerHTML += `<div class="invalido">Número Inválido: fale um numero entre ${menorValor} e ${maiorValor} !</div>`;
+        return
     }
 
     if(numero === numeroSecreto){
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente!</button>
+        `;
+    } else if(numero > numeroSecreto){
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-chevron-down"></i></div>
+        `;
+    } else if(numero < numeroSecreto){
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-chevron-up"></i></div>
         `;
     }
 }
@@ -24,3 +36,9 @@ function chuteInvalido(numero) {
 function numeroInvalido(numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id = "jogar-novamente"){
+        window.location.reload();
+    }
+})
