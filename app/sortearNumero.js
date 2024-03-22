b0ce74e5d895cf87efede467353532164f18d2ae
Jandir Moreira <jandirweb@gmail.com>
2024-03-22 14:43:58 -0300

    atualizando menor e maior valores e mathrandom

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,1 +1,13 @@
-console.log('funciona');
+const elementoMenorValor = document.querySelector('#menor-valor');
+const elementoMaiorValor = document.querySelector('#maior-valor');
+
+const menorValor = 1;
+const maiorValor = 100;
+const numeroSecreto = gerarNumeroAleatorio();
+
+elementoMenorValor.innerHTML = menorValor;
+elementoMaiorValor.innerHTML = maiorValor;
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * maiorValor + 1)
+}
